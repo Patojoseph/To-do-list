e7baf383c9a2f7a6c93be0f79e652896d542fbba
José Francisco <josefsp0508@gmail.com>
2025-03-26 17:37:36 -0300

    verificação de campos
    iniciando a verificação de campos com js

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,11 +1,22 @@
 //Fazer a passagem para dentro do site
 //com a falta de um banco não posso fazer as validações
 
-const submit = document.querySelector('#submit_form').onclick = function () {
-    document.querySelector('form').action = "./agenda.html"
+const submit = document.querySelector('#submit_form').onclick = function (e) {
+    //proibindo o envio caso esteja com algo errado
+    e.preventDefault();
+
+    //validando o nome do cliente
+    let Select_name = document.querySelector("#input_nome");
+    let Name = (Select_name.value);
+    //vou utilizar keycodes para fazer a verificação e exclusão das téclas que não quero
+    
+    console.log(Name)
+    
+    
+    
+    
+    
+    
+    //envio para o ínico para poder entrar com a conta criada
+    //document.querySelector('form').action = "index.html";
 }
-
-
-const Validation = document.querySelectorAll('.required')
-console.log(Validation.length)
-//fazer a verificação de todos os modos possíveis.
