c9e80b55d17b79edce63360fcd192cf246a32470
José Francisco <josefsp0508@gmail.com>
2025-03-27 16:56:44 -0300

    Adicionando verificações no cadastro
    Melhorando e incrementando a verificação no cadastro do site de agenda

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,5 +1,7 @@
 //Fazer a passagem para dentro do site
 //com a falta de um banco não posso fazer as validações
+//trocar essa variavel para uma função simples não precisa ser dessa forma, acho que com a função vai ficar bem mais simples
+
 
 const submit = document.querySelector('#submit_form').onclick = function (e) {
     //proibindo o envio caso esteja com algo errado
@@ -8,9 +10,30 @@ const submit = document.querySelector('#submit_form').onclick = function (e) {
     //validando o nome do cliente
     let Select_name = document.querySelector("#input_nome");
     let Name = (Select_name.value);
-    //vou utilizar keycodes para fazer a verificação e exclusão das téclas que não quero
-    
-    console.log(Name)
+    for(let i = 0; i < Name.length; i++){
+        let tecla = Name[i];
+        if (tecla == "1" || tecla == "2"|| tecla == "3"|| tecla == "4"|| tecla == "5"|| tecla == "6"|| tecla == "7"|| tecla == "8"|| tecla == "9"|| tecla == "0"){
+            alert("Não pode números no nome")
+            document.getElementById("input_nome").setAttribute("class", "error");
+            e.preventDefault();
+        };
+    };
+
+    //validando o cpf da pessoa 
+    let Select_cpf = document.querySelector("#cpf");
+    let cpf = (Select_cpf.value);
+    //verificação do formato, verificando os 3 primeiros digitos.
+    for( let i = 0; i < cpf[3]; i++){
+        if (cpf[i] == "-"){
+            if(i !== 3){
+                console.log("forma errada de escrita");
+            };
+        };
+    };
+
+    //tirando os - e . para fazer a verificação
+    cpf = cpf.replace(/\.|-/g,"");
+
     
     
     
